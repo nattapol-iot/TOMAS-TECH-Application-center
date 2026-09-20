d8b6d7da5a2930afb0b3d81aa79fe05842fc8bc3
nattapol-iot <nattapol.tomastech@gmail.com>
2026-09-20 11:51:54 +0700

    fix(deploy): defer CRM end user index compilation

--- database/migrations/054_crm_end_user.sql
+++ database/migrations/054_crm_end_user.sql
@@ -6,7 +6,7 @@ IF COL_LENGTH('dbo.crm_opportunities','end_user_customer_id') IS NULL
     CONSTRAINT FK_crm_opportunities_end_user_customer REFERENCES dbo.customers(id);
 
 IF NOT EXISTS(SELECT 1 FROM sys.indexes WHERE object_id=OBJECT_ID('dbo.crm_opportunities') AND name='IX_crm_opportunities_end_user')
-  CREATE INDEX IX_crm_opportunities_end_user ON dbo.crm_opportunities(end_user_customer_id) WHERE end_user_customer_id IS NOT NULL;
+  EXEC(N'CREATE INDEX IX_crm_opportunities_end_user ON dbo.crm_opportunities(end_user_customer_id) WHERE end_user_customer_id IS NOT NULL;');
 
 IF NOT EXISTS(SELECT 1 FROM dbo.schema_versions WHERE version=54)
   INSERT dbo.schema_versions(version,name) VALUES(54,N'CRM opportunity end user company');
